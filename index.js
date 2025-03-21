e1f419152be28305b39f67ddae3ce4373c77e7ac
Nehemie-Jacques <nehemiesighe1@gmail.com>
2025-03-21 20:09:33 +0100

    flags

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-//2
+//1
 let body = document.querySelector("body")
+let liste = document.createElement("ul")
+body.appendChild(liste)
+for ( let i= 0 ; i<=3  ; i++){
+    let l = document.createElement("li")
+    l.textContent = "hello world" 
+    l.className = "item-" +i
+    liste.appendChild(l)
+
+}
+let t = "hello world"
+let s = document.querySelector("li")
+for ( let i= 0 ; i<=3  ; i++){
+    let l = document.createElement("li")
+    l.textContent = `${i}-hello world`
+    l.className = "item-" +i
+    liste.appendChild(l)
+
+}
+
+//2
 for ( let i= 1 ; i<=10  ; i++){
     let p = document.createElement("p")
     p.textContent = "Paragraphe " + i
