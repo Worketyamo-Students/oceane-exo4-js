f271aa8674f3a62390aae44a272f6e8ef53bd389
Nehemie-Jacques <nehemiesighe1@gmail.com>
2025-03-26 20:04:53 +0100

    changement de page

--- dom.js
+++ dom.js
@@ -1,9 +1,13 @@
 let ul = document.getElementById("nombre")
+let list = ul
 let li = document.getElementById("modifier")
 let current = li
 currentnext = current.nextElementSibling
 currentnext.style.backgroundColor = "red"
 currentprevious = current.previousElementSibling
 currentprevious.style.backgroundColor = "yellow"
-alert(current.firstElementChild)
-alert(current.lastElementChild)
+console.log(list.firstElementChild.textContent)
+console.log(list.lastElementChild.textContent)
+current.addEventListener("click", (e) =>{
+    console.log(e.target)
+})
