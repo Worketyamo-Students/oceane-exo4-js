700ed1e20449478a4b71ec220b72a703c111c44b
Nehemie-Jacques <nehemiesighe1@gmail.com>
2025-03-27 20:03:08 +0100

    changement de page

--- dom.js
+++ dom.js
@@ -1,13 +1,31 @@
+
+
 let ul = document.getElementById("nombre")
-let list = ul
+let div = document.querySelector("div")
+let d = div
+let items = ul.getElementsByTagName("li")
 let li = document.getElementById("modifier")
+console.log(items.re)
+//3.1
 let current = li
 currentnext = current.nextElementSibling
-currentnext.style.backgroundColor = "red"
+currentnext.style.backgroundColor = "green"
 currentprevious = current.previousElementSibling
 currentprevious.style.backgroundColor = "yellow"
-console.log(list.firstElementChild.textContent)
-console.log(list.lastElementChild.textContent)
-current.addEventListener("click", (e) =>{
-    console.log(e.target)
-})
+//3.2
+console.log(ul.firstElementChild.textContent)
+console.log(ul.lastElementChild.textContent)
+// current.addEventListener("click", (e) =>{
+//     console.log(e.target)
+// })
+//3.3
+//3.5
+
+let p = document.getElementById("paragraph")
+let p_prime = p.cloneNode(true)
+div.insertBefore(p_prime , p.nextSibling)
+//3.$
+let p_prime1 = p.cloneNode(true)
+//exercice $
+    
+
